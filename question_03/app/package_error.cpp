cd1c956eb471089e4975c08ba2e4ac770c8ffe6a
IttaloSantos <ittalosantoss@gmail.com>
2024-02-05 13:06:08 -0300

    Add package error test

--- question_03/app/package_error.cpp
+++ question_03/app/package_error.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main()
 {
-    States current_state = process_event(LINK_DOWN, INTERFACE_OK);
+    States current_state = process_event(SENDING_START, NOK_PACKAGE_RECEIVED);
     cout << current_state << endl; // Expects SENDING_START - 2
 
-    current_state = process_event(current_state, OK_PACKAGE_RECEIVED);
+    current_state = process_event(SENDING_CONFIG, NOK_PACKAGE_RECEIVED);
     cout << current_state << endl; // Expects SENDING_CONFIG - 3
 
-    current_state = process_event(current_state, OK_PACKAGE_RECEIVED);
-    cout << current_state << endl; // KEEP_ALIVE - 4
+    current_state = process_event(KEEP_ALIVE, NOK_PACKAGE_RECEIVED);
+    cout << current_state << endl; // Expects SENDING_START - 2
 
-    current_state = process_event(current_state, OK_PACKAGE_RECEIVED);
-    cout << current_state << endl; // KEEP_ALIVE - 4
+    current_state = process_event(LINK_DOWN, NOK_PACKAGE_RECEIVED);
+    cout << current_state << endl; // LINK DOWN - 1
 
     return 0;
 }
