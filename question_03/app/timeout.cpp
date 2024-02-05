3b715a9b61076be07a65117dc58227265567a8ed
IttaloSantos <ittalosantoss@gmail.com>
2024-02-05 13:06:20 -0300

    Add timeout tests

--- question_03/app/timeout.cpp
+++ question_03/app/timeout.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main()
 {
-    States current_state = process_event(LINK_DOWN, INTERFACE_OK);
+    States current_state = process_event(LINK_DOWN, TIMEOUT);
+    cout << current_state << endl; // Expects LINK_DOWN - 1
+
+    current_state = process_event(SENDING_START, TIMEOUT);
     cout << current_state << endl; // Expects SENDING_START - 2
 
-    current_state = process_event(current_state, OK_PACKAGE_RECEIVED);
-    cout << current_state << endl; // Expects SENDING_CONFIG - 3
+    current_state = process_event(SENDING_CONFIG, TIMEOUT);
+    cout << current_state << endl; // Expects SENDING_START - 2
 
-    current_state = process_event(current_state, OK_PACKAGE_RECEIVED);
-    cout << current_state << endl; // KEEP_ALIVE - 4
-
-    current_state = process_event(current_state, OK_PACKAGE_RECEIVED);
-    cout << current_state << endl; // KEEP_ALIVE - 4
+    current_state = process_event(KEEP_ALIVE, TIMEOUT);
+    cout << current_state << endl; // Expects SENDING_START - 2
 
     return 0;
 }
